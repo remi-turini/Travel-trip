973528132cc26198c662a51b5e85418b132b5897
remi-turini <71329993+remi-turini@users.noreply.github.com>
2022-05-13 15:14:43 +0200

    cors

--- Back/app.js
+++ Back/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 const usersRoute = require("./routes/user");
 const loginRoute = require("./routes/login");
 
-app.use(bodyParser.json());
+app.use(bodyParser.json(), cors);
 
 app.use("/user", usersRoute);
 app.use("/login", loginRoute);
